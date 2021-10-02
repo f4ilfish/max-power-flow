6ea2cc18d478087ecfabdf596aae0bc790ce5e59
f4ilfish <mishka4ernij@mail.ru>
2021-10-02 14:59:34 +0700

    add console input

--- main.py
+++ main.py
@@ -1,24 +1,59 @@
+import sys
+
 import data_extract
 import file_creating
 import calc_criteria
 
+print("Hello. Let`s calculate maximum power flow (MPF)")
 
-# Считываем файлы
-flowgate_lines = data_extract.json_to_dic('flowgate.json')
-faults_lines = data_extract.json_to_dic('faults.json')
-trajectory_nodes = data_extract.csv_to_list('vector.csv')
+# Select path to flowgate .json
+print("Select path to flowgate .json:")
+try:
+    flowgate_lines = data_extract.json_to_dic(input())
+except Exception:
+    print("Unknown path, type of file or folder does not contain a file")
+    exit()
 
-# Создаем файлы
+# Select path to faults .json
+print("Select path to faults .json:")
+try:
+    faults_lines = data_extract.json_to_dic(input())
+except Exception:
+    print("Unknown path, type of file or folder does not contain a file")
+    exit()
+
+# Select path to trajectory .csv
+print("Select path to trajectory .csv:")
+try:
+    trajectory_nodes = data_extract.csv_to_list(input())
+except Exception:
+    print("Unknown path, type of file or folder does not contain a file")
+    exit()
+
+# Input power fluctuation
+print("Input positive power fluctuations:")
+try:
+    p_fluctuations = int(input())
+except Exception:
+    print("Input positive number")
+    exit()
+
+# Creating RastrWin3 files
 file_creating.do_sch(flowgate_lines, 'new')
 file_creating.do_ut2(trajectory_nodes)
 
-# Считаем режим
-print(calc_criteria.criteria1(30))
-print(calc_criteria.criteria2(30))
-print(calc_criteria.criteria3(30, faults_lines))
-print(calc_criteria.criteria4(faults_lines))
-print(calc_criteria.criteria5(flowgate_lines))
-print(calc_criteria.criteria6(faults_lines, flowgate_lines))
-
-
+# Calcutale criterias
+print(f"MPF in normal scheme (0.8*Pmax): "
+      f"{calc_criteria.criteria1(p_fluctuations)}")
+print(f"MPF by voltage in normal scheme (1,15*Ucr): "
+      f"{calc_criteria.criteria2(30)}")
+print(f"MPF in after emergency scheme (0.92*Pmax): "
+      f"{calc_criteria.criteria3(30, faults_lines)}")
+print(f"MPF by voltage in after emergency scheme (1.1*Ucr): "
+      f"{calc_criteria.criteria4(faults_lines)}")
+print(f"MPF by current in normal scheme (I - allowable): "
+      f"{calc_criteria.criteria5(flowgate_lines)}")
+print(f"MPF by current in after emergency scheme (I - critical): "
+      f"{calc_criteria.criteria6(faults_lines, flowgate_lines)}")
+print()
 
